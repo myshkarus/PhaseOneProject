b180184693af4a442fbbbbffea5ba9db85251551
Mykhailo Shpilienko <m.shpilenko@gmail.com>
2022-08-21 08:03:59 +0300

    Initial commit

---
+++ AmazonProducts.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ * @author Mykhailo Shpilienko
+ * @since Aug. 2022
+ */
+public class AmazonProducts {
+
+	/**
+	 *
+	 * @param args 
+	 * @author Mykhailo Shpilienko
+	 * @since Aug. 2022
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
